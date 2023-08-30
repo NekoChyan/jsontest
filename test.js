47ff9e283958cb4c4829044dc209f9972dda5622
NekoChyan <k25842895@gmail.com>
2023-08-30 23:49:04 +0800

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@
             eid: "IEC120698",
             token: "C9479D1051F34293AEE4B6BB246323",
             device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
-            para: "<dt>2023/09/01</dt><rid>11747</rid>",
+            para: "<dt>2023/09/01</dt><rid>117473</rid>",
             tp: 2
         })
     })
