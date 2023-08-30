20c3da1848af35f726022f412d7b9947a05ac6fa
NekoChyan <k25842895@gmail.com>
2023-08-30 23:36:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,14 +1,22 @@
-function ExchangeProduct() {
-	url: 'https://app.inventec.com/iservice/iServicePWA.asmx/doRouteBook'
-   		headers: {
-               		'Content-Type': 'application/json'
-               	}
+(function() {
+    fetch('https://app.inventec.com/iservice/iServicePWA.asmx/doRouteBook', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify({
-			eid: "IEC120698",
-			token: "C9479D1051F34293AEE4B6BB246323",
-			device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
-			para: "<dt>2023/09/01</dt><rid>11747</rid>",
-			tp: 2
-	})	
-	
-}
+            eid: "IEC120698",
+            token: "C9479D1051F34293AEE4B6BB246323",
+            device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
+            para: "<dt>2023/09/01</dt><rid>11747</rid>",
+            tp: 2
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // 可以更改成你想要處理回傳資料的程式碼
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+})();
