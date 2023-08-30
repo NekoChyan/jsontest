c93d0153e93240199a98350ae15aefbefe400546
NekoChyan <k25842895@gmail.com>
2023-08-30 22:12:06 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+function ExchangeProduct() {
+	body: JSON.stringify({
+		eid: IEC120698,
+		token: C9479D1051F34293AEE4B6BB246323,
+		device: <token>C9479D1051F34293AEE4B6BB246323</token>,
+		para: <dt>2023/09/01</dt><rid>117476</rid>,
+		tp: 2
+	})	
+}
