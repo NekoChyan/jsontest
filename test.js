a5534bff64eee1da57193c09e453f3f1f96f1818
NekoChyan <k25842895@gmail.com>
2023-08-30 23:53:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,27 @@
 (function() {
-    fetch('https://app.inventec.com/iservice/iServicePWA.asmx/doRouteBook', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            eid: "IEC120698",
-            token: "C9479D1051F34293AEE4B6BB246323",
-            device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
-            para: "<dt>2023/09/01</dt><rid>117473</rid>",
-            tp: 2
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // 可以更改成你想要處理回傳資料的程式碼
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    var request = new XMLHttpRequest();
+    
+    request.open('POST', 'https://app.inventec.com/iservice/iServicePWA.asmx/doRouteBook', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    
+    var requestData = {
+        eid: "IEC120698",
+        token: "C9479D1051F34293AEE4B6BB246323",
+        device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
+        para: "<dt>2023/09/01</dt><rid>11747</rid>",
+        tp: 2
+    };
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var responseData = JSON.parse(request.responseText);
+                console.log(responseData); // 可以更改成你想要處理回傳資料的程式碼
+            } else {
+                console.error('Error:', request.status, request.statusText);
+            }
+        }
+    };
+    
+    request.send(JSON.stringify(requestData));
 })();
