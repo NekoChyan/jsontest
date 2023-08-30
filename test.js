b862eeb47cd6677d6c863dad13a63a68163d39a8
NekoChyan <k25842895@gmail.com>
2023-08-30 23:24:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 function ExchangeProduct() {
-	body: JSON.stringify({
-		eid: "IEC120698",
-		token: "C9479D1051F34293AEE4B6BB246323",
-		device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
-		para: "<dt>2023/09/01</dt><rid>117479</rid>",
-		tp: 2
+	url: 'https://app.inventec.com/iservice/iServicePWA.asmx/doRouteBook'
+   		headers: {
+               		'Content-Type': 'application/json'
+               	}
+        body: JSON.stringify({
+			eid: "IEC120698",
+			token: "C9479D1051F34293AEE4B6BB246323",
+			device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
+			para: "<dt>2023/09/01</dt><rid>11747</rid>",
+			tp: 2
 	})	
+	
 }
