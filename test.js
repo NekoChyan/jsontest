eb3ae80a398f0900fe17fa18ca8cd192ea6c24fc
NekoChyan <k25842895@gmail.com>
2023-08-30 22:36:40 +0800

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function ExchangeProduct() {
 		eid: "IEC120698",
 		token: "C9479D1051F34293AEE4B6BB246323",
 		device: "<token>C9479D1051F34293AEE4B6BB246323</token>",
-		para: "<dt>2023/09/01</dt><rid></rid>",
+		para: "<dt>2023/09/01</dt><rid>117479</rid>",
 		tp: 2
 	})	
 }
