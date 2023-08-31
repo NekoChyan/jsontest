6c9d8ee65b350c7dcd747e5354a01281cfcd1c91
NekoChyan <k25842895@gmail.com>
2023-08-31 21:04:12 +0800

    beer

---
+++ food.js
@@ -0,0 +1,87 @@
+let showNotification = true;
+let token = null;
+
+function surgeNotify(subtitle = '', message = '') {
+  $notification.post('SHEIN 簽到', subtitle, message, {
+    'url': ''
+  });
+};
+
+function handleError(error) {
+  if (Array.isArray(error)) {
+    console.log(`❌ ${error[0]} ${error[1]}`);
+    if (showNotification) {
+      surgeNotify(error[0], error[1]);
+    }
+  } else {
+    console.log(`❌ ${error}`);
+    if (showNotification) {
+      surgeNotify(error);
+    }
+  }
+}
+
+async function preCheck() {
+  return new Promise((resolve, reject) => {
+    const sheinToken = $persistentStore.read('SheinToken');
+    if (!sheinToken || sheinToken.length === 0) {
+      return reject(['檢查失敗 ‼️', '找不到 token']);
+    }
+    token = sheinToken;
+    return resolve();
+  });
+}
+
+async function checkIn() {
+  return new Promise((resolve, reject) => {
+    try {
+      const request = {
+        url: 'https://api-shein.shein.com/h5/check_in/check',
+        headers: {
+          'token': token,
+          'siteuid': 'iosshtw',
+        }
+      };
+      $httpClient.get(request, function (error, response, data) {
+        if (error) {
+          return reject(['簽到失敗 ‼️', '連線錯誤']);
+        } else {
+          if (response.status === 200) {
+            const obj = JSON.parse(data);
+            if (obj.info) {
+              if (obj.info.check_success === 1) {
+                return resolve(obj.info.daily_reward);
+              } else if (obj.info.check_success === 2) {
+                showNotification = false;
+                return reject(['簽到失敗 ‼️', '今日已簽到']);
+              } else {
+                return reject(['簽到失敗 ‼️', 'Error: ' + obj.info]);
+              }
+            } else {
+              return reject(['簽到失敗 ‼️', 'Error: ' + obj.info]);
+            }
+          } else {
+            return reject(['簽到失敗 ‼️', response.status]);
+          }
+        }
+      });
+    } catch (error) {
+      return reject(['簽到失敗 ‼️', error]);
+    }
+  });
+}
+
+(async () => {
+  console.log('ℹ️ SHEIN 自動簽到 v20230215.1');
+  try {
+    await preCheck();
+    console.log('✅ 檢查成功');
+    await checkIn();
+    console.log('✅ 簽到成功');
+
+    surgeNotify('簽到成功 ✅', '');
+  } catch (error) {
+    handleError(error);
+  }
+  $done();
+})();
---
+++ getoken.js
@@ -0,0 +1,54 @@
+let showNotification = true;
+
+// function surgeNotify(subtitle = '', message = '') {
+//   $notification.post('Inventec token', subtitle, message, { 'url': '' });
+// };
+
+// function handleError(error) {
+//   if (Array.isArray(error)) {
+//     console.log(`❌ ${error[0]} ${error[1]}`);
+//     if (showNotification) {
+//       surgeNotify(error[0], error[1]);
+//     }
+//   } else {
+//     console.log(`❌ ${error}`);
+//     if (showNotification) {
+//       surgeNotify(error);
+//     }
+//   }
+// }
+
+// function isManualRun(checkRequest = false, checkResponse = false) {
+//   if (checkRequest) {
+//     return !$request || ($request.body && JSON.parse($request.body).foo === 'bar');
+//   }
+//   if (checkResponse) {
+//     return !$response || ($response.body && JSON.parse($response.body).foo === 'bar');
+//   }
+//   return false;
+// }
+
+async function getToken() {
+  return new Promise((resolve, reject) => {
+    try {
+      const request = {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "eid": "IEC120698",
+          "pass": "$Melon7809",
+          "device": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+          "co": "Google Inc.",
+          "IsOnce": "",
+          "version": "1.0.0"})
+      };
+      $httpClient.post(request, function(error,response,data){
+        const bdata = $response.data;
+        console.log(bdata);
+      });
+    } catch (error) {
+      return reject(['保存失敗 ‼️', error]);
+    }
+  });
+}
