9bf56d8532bd89f86c7ca680c7411fb804c89992
xRogerProject <k25842895@gmail.com>
2023-09-02 02:16:56 +0800

    Update

--- Real-time-debug.js
+++ Real-time-debug.js
@@ -9,7 +9,7 @@
 	const _$ = new nobyda();
 	const r = await new Promise(e => {
 		_$.get({
-			url: 'http://192.168.31.241:5500/test.js' // LAN backend address
+			url: 'http://192.168.31.232:5500/test.js' // LAN backend address
 		}, (t, c, o) => {
 			if (c && c.status == 200 && o) {
 				_$.write(o, "local-cache");
